3c33e16e6acdb730a85c219c1a86d2158d9d7c8f
Mathias Immerkjaer <immerkjaer@gmail.com>
2021-10-06 19:51:36 +0200

    Implement server and client

---
+++ src/ImplCompute.java
@@ -0,0 +1,8 @@
+package src;
+
+public class ImplCompute implements Compute {
+
+    public void printMsg(String msg) {
+        System.out.println(msg);
+    }
+}
--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,32 @@
 package src;
 
-public class Server {
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
-    public static void main(String[] args) {
-        System.out.println("hello");
+public class Server extends ImplCompute {
+    public Server() {}
+
+    public static void main(String args[]) {
+//        System.setProperty("java.rmi.server.hostname", "127.0.0.1");
+
+        try {
+//            System.setProperty("java.rmi.server.hostname", "127.0.0.1");
+            ImplCompute engine = new ImplCompute();
+
+            // Exporting the object of implementation class
+            // (here we are exporting the remote object to the stub)
+            Compute stub = (Compute) UnicastRemoteObject.exportObject(engine, 0);
+
+            // Binding the remote object (stub) in the registry
+            Registry registry = LocateRegistry.getRegistry();
+
+            registry.rebind("RMI_test", stub);
+            System.err.println("Server ready");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
     }
 }
