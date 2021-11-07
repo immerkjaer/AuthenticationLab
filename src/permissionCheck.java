52f3278d0fa87dcc188ac2e67302bf96ebf85405
Tommy Jonas Rath <s194586@student.dtu.dk>
2021-11-07 11:48:58 +0100

    its something, i guess

--- src/permissionCheck.java
+++ src/permissionCheck.java
@@ -1,5 +1,7 @@
 package src;
 
+import java.util.ArrayList;
+
 public class permissionCheck {
 
     public static void main(String[] args) {
@@ -7,10 +9,32 @@ public class permissionCheck {
     }
 
     public boolean isPermitted(String name, String command){
+        //Read Json to get role of person
+        String role = getRole(name);
 
+        //Read Json to get list og permitted commands for role
+        ArrayList<String> permittedCommands = getPermissionsFromRole(role);
 
-
+        //if command is in list, return true, otherwise return false
+        for (String s : permittedCommands){
+            if (s.equals(command)){
+                return true;
+            }
+        }
         return false;
+    }
+
+    public String getRole(String name){
+        //some Json thingy here to get the role of the person with name
+        return "";
+    }
+
+    public ArrayList<String> getPermissionsFromRole(String role){
+        ArrayList<String> permittedCommands = new ArrayList<String>();
+
+        //some Json thingy to put permissions into Arraylist
+
+        return permittedCommands;
     }
 
 }
