ac881af98d31fecc9d9a41f344c365a7ca674143
Mathias <immerkjaer@gmail.com>
2021-11-19 14:19:29 +0100

    Remove test of json loads

--- src/Server.java
+++ src/Server.java
@@ -12,8 +12,6 @@ public class Server extends PrinterServer {
 //        System.setProperty("java.rmi.server.hostname", "127.0.0.1");
 
         try {
-            new ReadJson().read("src/ACL.json");
-
 //            System.setProperty("java.rmi.server.hostname", "127.0.0.1");
             PrinterServer server = new PrinterServer();
 
